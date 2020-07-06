bae8e70f51c28d95b0e921999ef8c3ace08e7b97
Luffy009 <49230449+Luffy009@users.noreply.github.com>
2020-07-06 13:50:08 +0200

    FIx code

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,30 +5,18 @@ import QuizEnd from './QuizEnd'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    render() {
-        const isQuizEnd = true;
-        if ((this.state.quiz_position - 1) === quizData.quiz_questions.length) {
-            return isQuizEnd
-        }
-
-        if (isQuizEnd === true) {
-            return (
-                <div>
-                    <QuizEnd />
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-                </div>
-            )
-        }
-    }
-
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 };
+    }
+
+    render() {
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+        return (
+            <div>
+                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+            </div>
+        )
     }
 }
 
