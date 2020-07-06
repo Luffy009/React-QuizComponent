4fca26322351adaf111d68bab3264433d7073395
Luffy009 <49230449+Luffy009@users.noreply.github.com>
2020-07-06 10:46:02 +0200

    Fix QuizQuestion issues

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,10 +8,6 @@ class Quiz extends Component {
         return (
             <div>
                 <QuizQuestion quiz_question='quizData.quiz_questions[this.state.quiz_position -1]' />
-                {/* <div className='QuizQuestion'>
-                    {quizData.quiz_questions[0].instruction_text}
-
-                </div> */}
             </div>
         )
     }
